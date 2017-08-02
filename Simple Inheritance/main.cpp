89277ed5ada24386735c41b4ebd9654d855453e7
htmlcssjavascriptcoursera <h1609397@mvrht.com>
2017-08-02 18:22:38 +0200

    Expanded simple inherance.

--- Simple Inheritance/main.cpp
+++ Simple Inheritance/main.cpp
@@ -1,4 +1,4 @@
-/*Not inherited by derived class:
+/**Not inherited by derived class:
 - constructors,
 - destructors,
 - copy constructors,
@@ -8,43 +8,56 @@ versions of these have to be written for derived class manually.*/
 
 using namespace std;
 
-class Enemy
+class Enemy //-----------------------------------------------------------
 {
 public:
-    Enemy();
-    void DisplayDamage() const;
+    Enemy(int damage = 10);
+    void virtual DisplayDamage() const;
+    void virtual LaughAtPlayer () const; // Member function to be inherited and overridden is declared as virtual.
 
 protected:
     int m_Damage;
 };
-/** - Constructor */
-Enemy::Enemy():
-        m_Damage(10)
+Enemy::Enemy(int damage):
+        m_Damage(damage)
 {}
-/** - Member Funcs */
 void Enemy::DisplayDamage() const
 {
-    cout << "Damage is: " << m_Damage << endl;
+    cout << "Enemy class's calling: Damage is: " << m_Damage << endl;
 }
-/** ----------- END ----------- */
+void Enemy::LaughAtPlayer() const // Virtual keyword is used only in declaration.
+{
+    cout << "The enemy class laughs at player." << endl;
+}
 
-class Boss : public Enemy
+class Boss : public Enemy  //--------------------------------------------
 {
 public:
-    Boss();
+    Boss(int damage = 20, int damageMultiplier = 3);
     void SpecialDamage() const;
+    void virtual DisplayDamage() const;
+    void virtual LaughAtPlayer() const; // In derived calls virtual method will be virtual as well, it is not required to put virtual keyword here, but it is good practice, as it reminds me that this derived method is indeed virtual in base class.
 
 private:
     int m_DamageMultiplier;
 };
-/** - Constructor */
-Boss::Boss():
-    m_DamageMultiplier(3)
+Boss::Boss(int damage, int damageMultiplier):
+    Enemy(damage), // I called Enemy class's constructor to fill the damage with derived class Boss's default value.
+    m_DamageMultiplier(damageMultiplier)
 {}
-/** - Member Funcs */
 void Boss::SpecialDamage() const
 {
     cout << "Boss's special damage is: " << m_DamageMultiplier * m_Damage << endl;
+}
+void Boss::DisplayDamage() const
+{
+    cout << "Boss invoked this method and calls Enemy's DisplayDamage ----- " << endl;
+    Enemy::DisplayDamage(); // I call I call the original part enemy class's member member function here, in this overridden method in boss class. So I have original part + overridden part is some text wrapping.
+    cout << "And the boss laughs afterwards!!! (This was boss calling.)" << endl;
+}
+void Boss::LaughAtPlayer() const
+{
+    cout << "The derived boss class's overridden function laugh at player!" << endl;
 }
 
 void HitReturnKey (void)
@@ -56,12 +69,14 @@ void HitReturnKey (void)
 
 int main()
 {
-    cout << "Creating enemy:" << endl;
+    cout << "Enemy:" << endl;
     Enemy enemy0;
+    enemy0.LaughAtPlayer();
     enemy0.DisplayDamage();
 
-    cout << "Creating the boss." << endl;
+    cout << "Boss:" << endl;
     Boss boss0;
+    boss0.LaughAtPlayer();
     boss0.DisplayDamage();
     /**
     After this line of code, I have a Boss object with an m_Damage data member equal
@@ -76,9 +91,6 @@ automatically called and the object gets an m_Damage data member with a value of
 finishes off the object by giving it an m_DamageMultiplier data member with a
 value of 3 . The reverse happens when a Boss object is destroyed at the end of the
 program. First, the Boss class destructor is called for the object, and then the
-336 Chapter 10
-n
-Inheritance and Polymorphism: Blackjack
 Enemy class destructor is called. Because I didn’t define destructors in this
 program, nothing special happens before the Boss object ceases to exist.
     */
@@ -88,23 +100,3 @@ program, nothing special happens before the Boss object ceases to exist.
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
